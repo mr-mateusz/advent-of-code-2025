8b79b0e02872dfa4c671c73d2f7e45c6b78d2d06
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2025-12-01 06:41:46 +0100

    day_01 part 2

--- day_01/solution.py
+++ day_01/solution.py
@@ -1,7 +1,7 @@
-def turn_dial(state: int, rotation: str) -> int:
+def turn_dial(initial_state: int, rotation: str) -> tuple[int, int]:
     lb = 0
     ub = 99
-    if not lb <= state <= ub:
+    if not lb <= initial_state <= ub:
         raise ValueError()
 
     if len(rotation) < 2:
@@ -13,16 +13,25 @@ def turn_dial(state: int, rotation: str) -> int:
 
     distance = int(rotation[1:])
 
+    zero_clicks = 0
+    state = initial_state
+
     if direction == 'L':
         state -= distance
         while state < lb:
             state += ub + 1
-        return state
+            zero_clicks += 1
+        if state == 0:
+            zero_clicks += 1
+        if initial_state == 0:
+            zero_clicks -= 1
+        return state, zero_clicks
 
     state += distance
     while state > ub:
         state -= ub + 1
-    return state
+        zero_clicks += 1
+    return state, zero_clicks
 
 
 if __name__ == '__main__':
@@ -30,12 +39,17 @@ if __name__ == '__main__':
 
     state = 50
     zero_cnt = 0
+    total_zero_clicks = 0
 
     with open(path, 'r') as f:
         for line in f:
-            state = turn_dial(state, line.strip())
+            state, zero_clicks = turn_dial(state, line.strip())
             if state == 0:
                 zero_cnt += 1
+            total_zero_clicks += zero_clicks
 
     # Part 1
     print(zero_cnt)
+
+    # Part 2
+    print(total_zero_clicks)
