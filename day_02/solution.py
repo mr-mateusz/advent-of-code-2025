7df4f30de13af5b3220eea460f3959f3d045d731
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2025-12-02 08:40:12 +0100

    day_02 part 2

--- day_02/solution.py
+++ day_02/solution.py
@@ -19,6 +19,29 @@ def find_invalid_ids(lb: int, ub: int) -> list[int]:
     return invalid_ids
 
 
+def find_invalid_ids_p2(lb: int, ub: int) -> list[int]:
+    # lengths of repeated sequences we will be looking for
+    min_seq_len = 1
+    max_seq_len = len(str(ub)) // 2
+
+    # min and max number of digits in an id
+    min_n_digits = len(str(lb))
+    max_n_digits = len(str(ub))
+
+    # Now it needs to be a set, because 1111 can be derived from 1 1 1 1 and 11 11
+    invalid_ids = set()
+    for seq_len in range(min_seq_len, max_seq_len + 1):
+        for seq in range(10 ** (seq_len - 1), 10 ** seq_len):
+            id_to_check = str(seq) + str(seq)
+            while len(id_to_check) < min_n_digits:
+                id_to_check += str(seq)
+            while len(id_to_check) <= max_n_digits:
+                if lb <= int(id_to_check) <= ub:
+                    invalid_ids.add(int(id_to_check))
+                id_to_check += str(seq)
+    return list(invalid_ids)
+
+
 if __name__ == '__main__':
     path = 'input.txt'
 
@@ -33,4 +56,11 @@ if __name__ == '__main__':
         invalid_ids_sum += sum(find_invalid_ids(*id_range))
 
     # Part 1
+    print(invalid_ids_sum)
+
+    invalid_ids_sum = 0
+    for id_range in id_ranges:
+        invalid_ids_sum += sum(find_invalid_ids_p2(*id_range))
+
+    # Part 2
     print(invalid_ids_sum)
