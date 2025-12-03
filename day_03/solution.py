8f7e7b0cf1c0592df48fd1c2af326ca0d7235510
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2025-12-03 06:57:06 +0100

    day_03 part 2

--- day_03/solution.py
+++ day_03/solution.py
@@ -1,25 +1,42 @@
 from typing import Sequence
 
 
-def find_largest_joltage(line: Sequence[int]) -> int:
-    first_digit = max(line[:-1])
-    first_digit_pos = line.index(first_digit)
+def find_largest_joltage(line: Sequence[int], num_batteries: int) -> int:
+    if len(line) < num_batteries:
+        raise ValueError
 
-    second_digit = max(line[first_digit_pos + 1:])
+    start = 0
+    remaining_batteries = num_batteries
 
-    return first_digit * 10 + second_digit
+    digits = []
+    while remaining_batteries:
+        end = remaining_batteries - 1
+        digit = max(line[start:len(line) - end])
+        digit_pos = start + line[start:].index(digit)
+
+        digits.append(digit)
+
+        start = digit_pos + 1
+        remaining_batteries -= 1
+
+    return int(''.join(map(str, digits)))
 
 
 if __name__ == '__main__':
     path = 'input.txt'
 
-    total_joltage = 0
+    total_joltage_2 = 0
+    total_joltage_12 = 0
     with open(path, 'r') as f:
         for line in f:
             line = line.strip()
             line = list(map(int, line))
 
-            total_joltage += find_largest_joltage(line)
+            total_joltage_2 += find_largest_joltage(line, 2)
+            total_joltage_12 += find_largest_joltage(line, 12)
 
     # Part 1
-    print(total_joltage)
+    print(total_joltage_2)
+
+    # Part 2
+    print(total_joltage_12)
