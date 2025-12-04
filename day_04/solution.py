229f7107e7aa574cf6aac431be13be53d53c66ca
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2025-12-04 06:49:30 +0100

    day_04 part 2

--- day_04/solution.py
+++ day_04/solution.py
@@ -1,4 +1,4 @@
-def neighbouring_rolls(pos: tuple[int, int], diagram: list[str]) -> int:
+def neighbouring_rolls(pos: tuple[int, int], diagram: list[list[str]]) -> int:
     row_num, col_num = pos
 
     max_row = len(diagram)
@@ -20,20 +20,37 @@ def neighbouring_rolls(pos: tuple[int, int], diagram: list[str]) -> int:
     return n_rolls
 
 
-def accessible_rolls_num(diagram: list[str]) -> int:
-    arn = 0
+def accessible_rolls_iteration(diagram: list[list[str]]) -> list[tuple[int, int]]:
+    ars = []
     for row_num, row in enumerate(diagram):
         for col_num, value in enumerate(row):
             if value == '@' and neighbouring_rolls((row_num, col_num), diagram) < 4:
-                arn += 1
-    return arn
+                ars.append((row_num, col_num))
+    return ars
+
+
+def accessible_rolls_total_num(diagram: list[list[str]]) -> int:
+    diagram = [r[:] for r in diagram]
+
+    arn = 0
+    while True:
+        ars = accessible_rolls_iteration(diagram)
+        if not ars:
+            return arn
+
+        arn += len(ars)
+        for ar in ars:
+            diagram[ar[0]][ar[1]] = '.'
 
 
 if __name__ == '__main__':
     path = 'input.txt'
 
     with open(path, 'r') as f:
-        data = [l.strip() for l in f.readlines()]
+        data = [list(l.strip()) for l in f.readlines()]
 
     # Part 1
-    print(accessible_rolls_num(data))
+    print(len(accessible_rolls_iteration(data)))
+
+    # Part 2
+    print(accessible_rolls_total_num(data))
